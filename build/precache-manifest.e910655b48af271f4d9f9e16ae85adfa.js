7b8e42ce29a1b1d18cbddd7a58e8c03ff1901fa6
Amirul Asyraf <58986949+asyraf-labs@users.noreply.github.com>
2020-07-24 20:25:13 +0800

    Add files via upload

---
+++ build/precache-manifest.e910655b48af271f4d9f9e16ae85adfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3add66c334267dd5640dd10df789622b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a6931483948cb633818",
+    "url": "/static/css/main.f0b2c85b.chunk.css"
+  },
+  {
+    "revision": "fd50c702eb9777f3fe69",
+    "url": "/static/js/2.0352b4b4.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.0352b4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6931483948cb633818",
+    "url": "/static/js/main.c019f2b9.chunk.js"
+  },
+  {
+    "revision": "77a1c54aa6069cd77baa",
+    "url": "/static/js/runtime-main.9b4a9e49.js"
+  }
+]);
